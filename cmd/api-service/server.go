dad0cb27a398a6d78a41136ca066121478f206e4
Rohith Pentaparthy <penta@umich.edu>
2021-02-13 18:15:40 -0500

    in service and not in service

--- cmd/api-service/server.go
+++ cmd/api-service/server.go
@@ -6,13 +6,18 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 )
 
 type Station struct {
 	StationName    string `json:"stationName"`
 	TotalDocks     int    `json:"totalDocks"`
+	StatusValue    string `json:"statusValue"`
+	StatusKey      int    `json:"statusKey"`
 	AvailableBikes int    `json:"availableBikes"`
 	StAddress1     string `json:"stAddress1"`
 }
@@ -22,18 +27,25 @@ type StationData struct {
 	StationBeanList []Station `json:"stationBeanList"`
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func homePage(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Welcome to the API home page!")
 }
 
-func getStations(w http.ResponseWriter, req *http.Request) {
+func getStations() []Station {
 	urlEndpoint := "https://feeds.citibikenyc.com/stations/stations.json"
-	req, err := http.NewRequest(http.MethodGet, urlEndpoint, nil)
-	if err != nil {
-		log.Fatal(err)
+	stationReq, urlErr := http.NewRequest(http.MethodGet, urlEndpoint, nil)
+	if urlErr != nil {
+		log.Fatal(urlErr)
 	}
 	client := http.Client{}
-	res, getErr := client.Do(req)
+	res, getErr := client.Do(stationReq)
 	if getErr != nil {
 		log.Fatal(getErr)
 	}
@@ -50,22 +62,103 @@ func getStations(w http.ResponseWriter, req *http.Request) {
 		log.Fatalf("unable to parse value: %q, error: %s", string(body), jsonErr.Error())
 	}
 	stations := stationData.StationBeanList
-	for i := 0; i < len(stations); i++ {
-		response := fmt.Sprintf("StationName: %s, TotalDocks: %d, AvailableBikes: %d, Address: %s\n", stations[i].StationName, stations[i].TotalDocks, stations[i].AvailableBikes, stations[i].StAddress1)
+	return stations
+}
+
+func getStartAndEndIndices(startResults int, endResults int, pageInfo string) (start int, end int) {
+	page, pageErr := strconv.Atoi(pageInfo)
+	if pageInfo != "" {
+		if pageErr != nil {
+			fmt.Println("error converting string to int")
+			return
+		}
+	}
+
+	if page > 0 && (page-1)*20 <= endResults {
+		startResults += (page - 1) * 20
+		endResults = min(startResults+20, endResults)
+	}
+	return startResults, endResults
+}
+
+func getAllStations(w http.ResponseWriter, req *http.Request) {
+	stations := getStations()
+
+	// u, err := router.Get("YourHandler").URL("id", id, "key", key)
+
+	startResults := 0
+	endResults := len(stations)
+	fmt.Println("GET params were:", req.URL.Query())
+	fmt.Println(req.URL)
+	pageInfo := req.URL.Query().Get("page")
+	fmt.Println(reflect.TypeOf(pageInfo))
+
+	startResults, endResults = getStartAndEndIndices(startResults, endResults, pageInfo)
+
+	for i := startResults; i < endResults; i++ {
+		response := fmt.Sprintf("%d: StationName: %s, TotalDocks: %d, StatusValue: %s, StatusKey: %d, AvailableBikes: %d, Address: %s\n", i, stations[i].StationName, stations[i].TotalDocks, stations[i].StatusValue, stations[i].StatusKey, stations[i].AvailableBikes, stations[i].StAddress1)
 		fmt.Fprintf(w, response)
 	}
 	fmt.Println(stations[0].TotalDocks)
 }
 
+func getInServiceStations(w http.ResponseWriter, req *http.Request) {
+	allStations := getStations()
+	var inServiceStations []Station
+	for _, v := range allStations {
+		if v.StatusValue == "In Service" {
+			inServiceStations = append(inServiceStations, v)
+		}
+	}
+
+	stations := inServiceStations
+	startResults := 0
+	endResults := len(stations)
+	pageInfo := req.URL.Query().Get("page")
+	startResults, endResults = getStartAndEndIndices(startResults, endResults, pageInfo)
+
+	for i := startResults; i < endResults; i++ {
+		response := fmt.Sprintf("%d: StationName: %s, TotalDocks: %d, StatusValue: %s, StatusKey: %d, AvailableBikes: %d, Address: %s\n", i, stations[i].StationName, stations[i].TotalDocks, stations[i].StatusValue, stations[i].StatusKey, stations[i].AvailableBikes, stations[i].StAddress1)
+		fmt.Fprintf(w, response)
+	}
+}
+
+func getNotInServiceStations(w http.ResponseWriter, req *http.Request) {
+	allStations := getStations()
+	var notInServiceStations []Station
+	for _, v := range allStations {
+		if v.StatusValue == "Not In Service" {
+			notInServiceStations = append(notInServiceStations, v)
+		}
+	}
+
+	stations := notInServiceStations
+	startResults := 0
+	endResults := len(stations)
+	pageInfo := req.URL.Query().Get("page")
+	startResults, endResults = getStartAndEndIndices(startResults, endResults, pageInfo)
+
+	for i := startResults; i < endResults; i++ {
+		response := fmt.Sprintf("%d: StationName: %s, TotalDocks: %d, StatusValue: %s, StatusKey: %d, AvailableBikes: %d, Address: %s\n", i, stations[i].StationName, stations[i].TotalDocks, stations[i].StatusValue, stations[i].StatusKey, stations[i].AvailableBikes, stations[i].StAddress1)
+		fmt.Fprintf(w, response)
+	}
+}
+
 func handleRequests() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", homePage)
-	mux.HandleFunc("/stations", getStations)
+	router := mux.NewRouter()
+	router.Methods("GET").Path("/stations").HandlerFunc(getAllStations)
+	router.Methods("GET").Path("/stations").Queries("path", "{[0-9]*?}").HandlerFunc(getAllStations)
+	router.Methods("GET").Path("/stations/in-service").HandlerFunc(getInServiceStations)
+	router.Methods("GET").Path("/stations/in-service").Queries("path", "{[0-9]*?}").HandlerFunc(getInServiceStations)
+	router.Methods("GET").Path("/stations/not-in-service").HandlerFunc(getNotInServiceStations)
+	router.Methods("GET").Path("/stations/not-in-service").Queries("path", "{[0-9]*?}").HandlerFunc(getNotInServiceStations)
 
 	n := negroni.Classic() // Includes some default middlewares
-	n.UseHandler(mux)
+	n.UseHandler(router)
 
-	log.Fatal(http.ListenAndServe(":4000", n))
+	if err := http.ListenAndServe(":4000", n); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ropenta/golang-api
 
 go 1.15
 
-require github.com/urfave/negroni v1.0.0 // indirect
+require (
+	github.com/go-delve/delve v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/urfave/negroni v1.0.0
+)
